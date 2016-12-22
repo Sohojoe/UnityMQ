6d4b01b16d38e1a688a7d81abf87c31e813ea7fb
Joe Booth <joe@joebooth.com>
2016-12-21 21:16:50 -0800

    ReqRep example

---
+++ Assets/UnityMQExamples/ReqResExample/RequestExample.cs
@@ -0,0 +1,76 @@
+﻿// based on: https://github.com/zeromq/netmq/issues/526#issuecomment-230193595
+
+using UnityEngine;
+using System.Threading;
+using System.Collections;
+using System.Timers;
+using NetMQ; // for NetMQConfig
+using NetMQ.Sockets; 
+
+public class RequestExample: MonoBehaviour
+{
+	string debug_name_ = "Request: ";
+
+	Thread client_thread_;
+	private Object thisLock_ = new Object();
+	bool stop_thread_ = false;
+
+	void Log(string str)
+	{
+		Debug.Log (debug_name_ + str);
+	}
+
+	void Start()
+	{
+		Log(debug_name_ + "Start a request thread.");
+		client_thread_ = new Thread(NetMQClient);
+		client_thread_.Start();
+	}
+
+	// Client thread which does not block Update()
+	void NetMQClient()
+	{
+		AsyncIO.ForceDotNet.Force();
+		NetMQConfig.ManualTerminationTakeOver();
+		NetMQConfig.ContextCreate(true);
+
+		string msg;
+		var timeout = new System.TimeSpan(0, 0, 1); //1sec
+
+		Log("Connect to the server.");
+		var socket = new RequestSocket(">tcp://localhost:5557");
+
+		while (true)
+		{
+			lock (thisLock_) {
+				if (stop_thread_)
+					break;
+			}
+//			Log("Send Request.");
+			socket.SendFrame("hello");
+			if (socket.TryReceiveFrameString (timeout, out msg)) {
+				Log ("recived: " + msg);
+			} else {
+				Log ("Timed out, sleep");
+				Thread.Sleep (1000);
+			}
+		}
+
+		socket.Close();
+		Log("ContextTerminate.");
+		NetMQConfig.ContextTerminate();
+	}
+
+	void Update()
+	{
+		/// Do normal Unity stuff
+	}
+
+	void OnApplicationQuit()
+	{
+		lock (thisLock_)stop_thread_ = true;
+		client_thread_.Join();
+		Log("Quit the thread.");
+	}
+
+}
---
+++ Assets/UnityMQExamples/ReqResExample/ResponseExample.cs
@@ -0,0 +1,75 @@
+﻿// based on: https://github.com/zeromq/netmq/issues/526#issuecomment-230193595
+
+using UnityEngine;
+using System.Threading;
+using System.Collections;
+using System.Timers;
+using NetMQ; // for NetMQConfig
+using NetMQ.Sockets; 
+
+public class ResponseExample: MonoBehaviour
+{
+	string debug_name_ = "Response: ";
+		
+	Thread client_thread_;
+	private Object thisLock_ = new Object();
+	bool stop_thread_ = false;
+
+	void Log(string str)
+	{
+		Debug.Log (debug_name_ + str);
+	}
+
+	void Start()
+	{
+		Log(debug_name_ + "Start a request thread.");
+		client_thread_ = new Thread(NetMQClient);
+		client_thread_.Start();
+	}
+
+	// Client thread which does not block Update()
+	void NetMQClient()
+	{
+		AsyncIO.ForceDotNet.Force();
+		NetMQConfig.ManualTerminationTakeOver();
+		NetMQConfig.ContextCreate(true);
+
+		string msg;
+		var timeout = new System.TimeSpan(0, 0, 1); //1sec
+
+		Log("Connect to the server.");
+		var socket = new ResponseSocket("@tcp://localhost:5557");
+		while (true)
+		{
+			lock (thisLock_) {
+				if (stop_thread_)
+					break;
+			}
+//			Log("Get request.");
+			if (socket.TryReceiveFrameString (timeout, out msg)) {
+				Log ("recived: " + msg);
+				socket.SendFrame ("world");
+			} else {
+				Log ("Timed out, sleep");
+				Thread.Sleep (1000);
+			}
+		}
+
+		socket.Close();
+		Log("ContextTerminate.");
+		NetMQConfig.ContextTerminate();
+	}
+
+	void Update()
+	{
+		/// Do normal Unity stuff
+	}
+
+	void OnApplicationQuit()
+	{
+		lock (thisLock_)stop_thread_ = true;
+		client_thread_.Join();
+		Log("Quit the thread.");
+	}
+
+}
