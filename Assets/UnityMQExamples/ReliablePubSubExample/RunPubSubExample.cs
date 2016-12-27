91c2fc17ff9e1c2d526c0e6978bbce6229390850
Joe Booth <joe@joebooth.com>
2016-12-26 21:02:28 -0800

    Closedown threads

--- Assets/UnityMQExamples/ReliablePubSubExample/RunPubSubExample.cs
+++ Assets/UnityMQExamples/ReliablePubSubExample/RunPubSubExample.cs
@@ -14,12 +14,17 @@ public class RunPubSubExample: MonoBehaviour {
 		Debug.Log (debug_name_ + str);
 	}
 
+	Thread _server_thread;
+	Thread _client_thread;
+	object _threadLock = new object();
+	bool _endThread;
+
 	// Use this for initialization
 	void Start () {
 		AsyncIO.ForceDotNet.Force();
 		NetMQConfig.ManualTerminationTakeOver();
 		NetMQConfig.ContextCreate(true);
-		var serverTread = new Thread(() =>
+		var _server_thread = new Thread(() =>
 			{
 				Log("Start server.");
 				var _server = new ReliableServer("tcp://*:6669");
@@ -28,6 +33,10 @@ public class RunPubSubExample: MonoBehaviour {
 
 				while (true)
 				{
+					lock (_threadLock) {
+						if (_endThread)
+							break;
+					}
 					var message = new NetMQMessage();
 					message.Append("A");
 					message.Append("hello: " + index);
@@ -36,11 +45,12 @@ public class RunPubSubExample: MonoBehaviour {
 					Thread.Sleep(100);
 					index++;
 				}
-
+				Log("close server.");
+				_server.Dispose();
 			});
-		serverTread.Start();
+		_server_thread.Start();
 
-		var clientTread = new Thread(() =>
+		var _client_thread = new Thread(() =>
 			{
 				Log("Start client.");
 				var _client = new ReliableClient("tcp://localhost:6669");
@@ -48,6 +58,11 @@ public class RunPubSubExample: MonoBehaviour {
 
 				while (true)
 				{
+					lock (_threadLock) {
+						if (_endThread)
+							break;
+					}
+
 					NetMQMessage msg = null;
 					msg = _client.ReceiveMessage();
 					if (msg != null)
@@ -59,12 +74,31 @@ public class RunPubSubExample: MonoBehaviour {
 					}
 					Thread.Sleep(10);
 				}
+				Log("close client.");
+				_client.Dispose();
 			});
-		clientTread.Start();
+		_client_thread.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnApplicationQuit()
+	{
+		lock (_threadLock)
+			_endThread = true;
+		Thread.Sleep(200);	
+		if (_server_thread != null) {
+			_server_thread.Join();
+			_server_thread = null;
+		}
+		if (_client_thread != null) {
+			_client_thread.Join();
+			_client_thread = null;
+		}
+
+		Log("Quit the thread.");
 	}
 }
